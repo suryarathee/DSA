5692d129b93120789916d8f8d3b4f910cd4a87cd
suryarathee <ratheesurya23@gmail.com>
2025-03-31 08:59:22 +0530

    changes

--- Searching & Sorting/Bubble_sort.cpp
+++ Searching & Sorting/Bubble_sort.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void bubble_sort(vector<int> & nums)
 {
     int n = nums.size();
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
         int flag = true;
         for(int j= 0 ;j<n-i;j++)

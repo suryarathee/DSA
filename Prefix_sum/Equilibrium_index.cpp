48245bb621f95db9e2012fe5535517ba6dc19e50
suryarathee <ratheesurya23@gmail.com>
2025-06-11 12:22:20 +0530

    optimial equilibrium

--- Prefix_sum/Equilibrium_index.cpp
+++ Prefix_sum/Equilibrium_index.cpp
@@ -1,6 +1,7 @@
 /*An Equilibrium index is an index in an arr where sum of elements at lower indices is equal to sum of elements at higher indices*/
 #include<iostream>
 #include<vector>
+#include<numeric>
 using namespace std;
 /*Implementation using prefixsum for O(n) time complexity and O(n) space complexity*/
 int equilibrium_index(vector<int> arr)
@@ -20,11 +21,31 @@ int equilibrium_index(vector<int> arr)
             return i;
         }
     }
+    return -1;
+}
+/* IMplementation using prefixsum for Time Complexity:O(n) and Space Comlexity O(1)*/
+int optimal_equilibrium_index(vector<int> arr)
+{
+    int sum = accumulate(arr.begin(),arr.end(),0);
+    int n = arr.size();
+    for(int i=1;i<n;i++)
+    {
+        arr[i] = arr[i]+arr[i-1];
+    }
+    if(arr[0] == sum) return 0;
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i-1]==(sum-arr[i]))
+        {
+            return i;
+        }
+    }
+    return -1;
 }
 
 int main()
 {
     vector<int> arr = {1,3,0,4,-1,1};
-    cout<<equilibrium_index(arr);
+    cout<<optimal_equilibrium_index(arr);
     return 0;
 }
